9064ec3b0529152e4ff22ca7ab6f7bc0c3bd10fa
cooper <pbg0205@naver.com>
2024-12-24 01:47:05 +0900

    first commit

---
+++ src/main/java/com/cooper/lecture2024/Lecture2024Application.java
@@ -0,0 +1,13 @@
+package com.cooper.lecture2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooper/lecture2024/Lecture2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.lecture2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
