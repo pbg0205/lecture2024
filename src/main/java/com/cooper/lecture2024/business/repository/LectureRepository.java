6e5b5fc75050ceff8dde83131c92ea8e3ea826d3
cooper <pbg0205@naver.com>
2024-12-25 15:08:00 +0900

    feat: 수강 신청 개발 - LectureRepositoryImpl

--- src/main/java/com/cooper/lecture2024/business/repository/LectureRepository.java
+++ src/main/java/com/cooper/lecture2024/business/repository/LectureRepository.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.cooper.lecture2024.business.dto.response.LectureQueryResult;
+import com.cooper.lecture2024.domain.Lecture;
 
 public interface LectureRepository {
 	List<LectureQueryResult> findLecturesByStartAtBetween(
 		final LocalDateTime startDateTime, final LocalDateTime deadLineToStart);
+
+	Lecture findById(Long lectureId);
 }
--- src/test/java/com/cooper/lecture2024/business/repository/LectureRepositoryTest.java
+++ src/test/java/com/cooper/lecture2024/business/repository/LectureRepositoryTest.java
@@ -15,11 +15,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.jdbc.Sql;
 
 import com.cooper.lecture2024.business.dto.response.LectureQueryResult;
+import com.cooper.lecture2024.domain.Lecture;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ComponentScan(value = {"com.cooper.lecture2024.common.jpa", "com.cooper.lecture2024.infra.rdb"})
-@Sql(scripts = {"classpath:sql/lecture_sample.sql", "classpath:sql/lecturer_sample.sql"})
 class LectureRepositoryTest {
 
 	@Autowired
@@ -27,6 +27,7 @@ class LectureRepositoryTest {
 
 	@Test
 	@DisplayName("[성공] 검색 날짜와 일치하는 강의 존재시, 목록 반환")
+	@Sql(scripts = {"classpath:sql/lecture_sample.sql", "classpath:sql/lecturer_sample.sql"})
 	void findLecturesByStartAtBetween() {
 		// given
 		final LocalDateTime startDateTime = LocalDateTime.of(2024, 12, 24, 0, 0);
@@ -60,5 +61,23 @@ class LectureRepositoryTest {
 
 		// then
 		assertThat(lectureQueryResultList.size()).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("[성공] 강의 식별자를 통해 강의 조회")
+	@Sql("classpath:sql/lecture_sample.sql")
+	void findById() {
+		// given
+		final long lectureId = 1L;
+
+		// when
+		final Lecture lecture = lectureRepository.findById(lectureId);
+
+		// then
+		assertSoftly(softAssertions -> {
+			softAssertions.assertThat(lecture.getId()).isEqualTo(lectureId);
+			softAssertions.assertThat(lecture.getRemainingCount()).isNotNegative();
+			softAssertions.assertThat(lecture.getRemainingCount()).isLessThanOrEqualTo(lecture.getCapacity());
+		});
 	}
 }
